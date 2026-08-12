95c9380a4c3ec29816961cea443c61fd21656a58
guilhermebarros2711 <guilhermebarros2711@gmail.com>
2026-08-12 20:46:32 +0000

    gfh

--- 3_BImestre/Aula_03_08/aula_04_08/navigation/StackNavigator.js
+++ 3_BImestre/Aula_03_08/aula_04_08/navigation/StackNavigator.js
@@ -1,8 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Login from '../screens/Login/login';
-import Home from '../screens/home/Home';
+import Login from '../screens/Login/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,17 +9,10 @@ export default function StackNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
         <Stack.Screen
           name="Login"
           component={Login}
         />
-
-        <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
